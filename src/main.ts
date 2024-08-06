34fac48ba6b3dcb41767a09c24c4ffb15228d18f
Ssuhyeong <tngud124@gmail.com>
2024-08-06 10:30:47 +0900

    📝 feat : 진입점에 필요한 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { RedisIoAdapter } from './adapters/redis.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = parseInt(process.env.SERVER_PORT) || 3000;
+
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+
+  const logger = new Logger('Main');
+
+  app.enableCors({ origin: true });
+  app.useWebSocketAdapter(redisIoAdapter);
+
+  await app.listen(PORT, () => {
+    logger.debug(`Server started on port ${PORT}`);
+  });
 }
 bootstrap();
